b8fd988d436146c67a9132e5158a2dede1f6f59c
Chris Yammine <chris.yammine@hotmail.com>
2014-07-10 18:57:26 -0700

    Style home page and change route to home#home

--- app/models/kents.rb
+++ app/models/kents.rb
@@ -1,2 +1,7 @@
 class Kents < ActiveRecord::Base
+
+  geocoded_by :address   # can also be an IP address  
+  after_validation :geocode          # auto-fetch coordinates
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   post 'eateries/:id' => "eateries#like", as: "eatery_like"
   post 'profiles/:id' => "profiles#like", as: "profile_like"
 
-  root 'eateries#index'
+  root 'home#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
