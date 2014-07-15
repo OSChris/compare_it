046f17510001a0656b62995ee675fee8d37be6cc
Chris Yammine <chris.yammine@hotmail.com>
2014-07-14 20:47:07 -0700

    testing check in at codecore

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -8,7 +8,7 @@ class CheckInsController < ApplicationController
     result = request.location
     @kents = Eatery.last
 
-    if @kents.distance_from(result.coordinates) <= 0.03
+    if @kents.distance_from(result.coordinates) <= 0.5
       @validation = current_user.build_kent_validation
       @validation.save
       redirect_to root_path, notice: "You're now verified! Your reviews will count towards eatery scores now!"
