3132e6c7b02658b6d34ebd35227ec95ecdcc966f
Chris Yammine <chris.yammine@hotmail.com>
2014-07-14 20:45:16 -0700

    testing check in at codecore

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -6,11 +6,12 @@ class CheckInsController < ApplicationController
 
   def geocheckin
     result = request.location
-    @kents = Kents.first
+    @kents = Eatery.last
 
     if @kents.distance_from(result.coordinates) <= 0.03
       @validation = current_user.build_kent_validation
       @validation.save
+      redirect_to root_path, notice: "You're now verified! Your reviews will count towards eatery scores now!"
     else
       flash.now[:alert] = "You aren't in Kent's Kitchen!"
       render :statuscheck
