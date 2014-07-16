cec9525b27ba86f5da291678457cd78a233a5c1b
Chris Yammine <chris.yammine@hotmail.com>
2014-07-15 18:28:17 -0700

    changed google oauth settings for production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,8 +234,8 @@ Devise.setup do |config|
   config.omniauth :twitter, Rails.application.secrets.twitter_api_key,
                             Rails.application.secrets.twitter_api_secret
 
-  config.omniauth :google_oauth2, Rails.application.secrets.GOOGLE_CLIENT_ID,
-                                  Rails.application.secrets.GOOGLE_CLIENT_SECRET
+  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID,"],
+                                  ENV["GOOGLE_CLIENT_SECRET"]
 
 
   # ==> Warden configuration
