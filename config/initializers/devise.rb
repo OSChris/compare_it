0930a55e4dc29b5ac77a1e1b4485dd8f8c96dc94
Chris Yammine <chris.yammine@hotmail.com>
2014-07-16 10:29:22 -0700

    fixed devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,7 +234,7 @@ Devise.setup do |config|
   config.omniauth :twitter, Rails.application.secrets.twitter_api_key,
                             Rails.application.secrets.twitter_api_secret
 
-  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID,"],
+  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"],
                                   ENV["GOOGLE_CLIENT_SECRET"]
 
 
