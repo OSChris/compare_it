aec8bcfd3e880014b5b5bd0aa97f70839677f051
Chris Yammine <chris@wishpond.com>
2014-09-21 13:35:12 -0700

    Fixing fields

--- app/models/wishpond_test.rb
+++ app/models/wishpond_test.rb
@@ -1,2 +1,6 @@
 class WishpondTest < ActiveRecord::Base
+
+  alias_attribute :first_name, :'first-name'
+  alias_attribute :last_name, :'last-name'
+
 end
--- db/migrate/20140921194632_change_name_fields_in_wishpond_tests.rb
+++
@@ -1,6 +0,0 @@
-class ChangeNameFieldsInWishpondTests < ActiveRecord::Migration
-  def change
-  	rename_column :wishpond_tests, :first_name, "first-name"
-  	rename_column :wishpond_tests, :last_name, "last-name"
-  end
-end
